979413af6f5616860038db5bd183bead6042c26e
Diego Agliati <dagliati@gmail.com>
2024-09-23 21:23:36 -0300

    commit 1

---
+++ src/components/NavBar/CartWidget.jsx
@@ -0,0 +1,12 @@
+import { FaCartArrowDown } from "react-icons/fa";
+
+const CartWidget = () => {
+  return (
+    <div className="cartwidget">
+        <FaCartArrowDown size={40} color="white" />
+        <p>1</p>
+    </div>
+  )
+}
+
+export default CartWidget

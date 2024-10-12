177e2de2b2c2e2759702968548305f79c5aa483f
Diego Agliati <dagliati@gmail.com>
2024-10-12 16:17:54 -0300

    Update v1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
+import NavBar from "./components/NavBar/NavBar"
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css'
-import NavBar from './components/NavBar/NavBar'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+
 function App() {
 
   return (
-    <div className='contenedor'>
-     <NavBar />
-     <ItemListContainer greeting={"Bienvenidos a la tienda"} />
+    <div className="app-container" >
+      <BrowserRouter>
+        <NavBar />
+
+        <Routes>
+          <Route path="/" element={ <ItemListContainer greeting="Bienvenidos a la tienda" /> } />
+          <Route path="/category/:idCategory" element={ <ItemListContainer greeting="Bienvenidos" /> } />
+          <Route path="/detail/:idProduct" element={<ItemDetailContainer />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
